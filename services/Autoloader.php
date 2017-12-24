314bbef6e7922c3b88cf51e745ff07d8cfd4ccc5
RomanKuzhekov <profet@inbox.ru>
2017-12-24 23:02:27 +0300

    Update Autoloader.php

--- services/Autoloader.php
+++ services/Autoloader.php
@@ -5,4 +5,15 @@
  * Date: 26.11.2017
  * Time: 23:00
  */
-spl_autoload_register();
+namespace services;
+
+class Autoloader
+{
+    function loadClass($classname){
+        $classname = str_replace("app\\", $_SERVER['DOCUMENT_ROOT']."/", $classname);
+        $classname = str_replace("\\","/",$classname.".php");
+        if(file_exists($classname)){
+            require $classname;
+        }
+    }
+}
